9b909c058de8e3857c0595d5cd707502f3fbaa62
andreshugueth <andreshugueth@gmail.com>
2020-09-14 16:18:47 -0500

    API task 1 username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/" + user_id)
     todo_keys = {"userId": user_id}
     tasks = requests.get("{}todos".format(url), params=todo_keys)
-    employee_name = user.json().get("name")
+    employee_name = user.json().get("username")
 
     with open('{}.csv'.format(user_id), mode='w') as csv_file:
         writer = csv.writer(csv_file,
